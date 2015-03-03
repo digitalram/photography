dc8d27a3e585e3721cebb127ea265c2fd6fcc1e3
Mike <themurf@gmail.com>
2015-03-03 10:18:35 -0600

    added file

---
+++ PhotographySESPRING2015/nice/admin/pages/manageSchedule.php
@@ -0,0 +1,34 @@
+<h2>Manage Schedule</h2>
+
+<script type="text/javascript">
+	$(function() {
+		$.post("scripts/general/loadOverview.php", {}, function(json) {
+			var data = $.parseJSON(json);
+
+			if(data.publish_schedule == false) {
+				$("#btnPublishSchedule").prop("value", "Unpublish Schedule");
+			}
+
+			// disable the button; also disable any table modification
+			if(data.create_schedule == false) {
+				$("#btnCreateSchedule").prop("disabled", true);
+			}
+
+			admin.getMaxTables();
+
+		});
+	});
+</script>
+
+<table>
+	<tr>
+		<td><button class="bigAButton" id="btnCreateSchedule" type="button" onclick="javascript:admin.createSchedule();">Create Schedule</button></td>
+		<td><button class="bigAButton" id="btnPublishSchedule" type="button" onclick="javascript:admin.publishSchedule();">Publish Schedule</button></td>
+		<td><button class="bigAButton" id="btnClearAttendees" type="button" onclick="javascript:admin.clearSchedule();">Clear Schedule
+		</button></td>
+	</tr>
+</table>
+
+<h3>Schedule Below...</h3>
+
+
