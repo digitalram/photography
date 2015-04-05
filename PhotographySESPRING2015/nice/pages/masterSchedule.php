05f5626cb42fb144463c1ea9b762456443c4c278
mpjovanovich <mpjovanovich@gmail.com>
2015-04-04 19:49:45 -0500

    Fixed issues with master schedule viewing and printing

--- PhotographySESPRING2015/nice/pages/masterSchedule.php
+++ PhotographySESPRING2015/nice/pages/masterSchedule.php
@@ -1,201 +1,70 @@
+<?php include_once "bootstrap.php"; ?>
+
+<!-- Write schedule header -->
+<h1 style="padding-left: 15px;"><?php echo "Master Schedule ". (($session == "") ? "" : " - " . $session); ?></h1>
+<p style="padding-left: 15px;"><a href="" onclick="window.print();">Print this Schedule (to download schedule, print as a PDF).</a></p>
+<hr />
+<div class="schedule">
+
 <?php
-
-	# note: same thing as reviewerSchedule.php
-
-	# fixme: pagination is fucky
-
-	include_once "bootstrap.php";
-
 	$registrationPeriod		= app::getCurrentRegistrationPeriod();
 	$registrationPeriodId	= $registrationPeriod["registration_period_id"];
 
-	// reqs
 	$request = framework::getRequestVars();
 
-	$typeSql = "";
-
-	if(isset($request["day"])) {
-		$typeSql = "
-		AND SUBSTR(s.day_slot,1,1) = '". strtoupper(substr($request["day"], 0, 1)) ."'
-		";
-	}
-
-	// pagination
-	$page	= (isset($request["p"])) ? $request["p"] : 1;
-	$limit	= 24 * 2;
-	$offset	= $page * $limit;
-
-	$total = framework::getMany("
-	SELECT
-		s.day_slot AS day_slot,
-		s.table_num AS table_num,
-		s.reviewer_id AS reviewer_id,
-		ur.first_name AS reviewer_firstname,
-		ur.last_name AS reviewer_lastname,
-		ua.first_name AS attendee_firstname,
-		ua.last_name AS attendee_lastname
+	$session_sql = 
+	"SELECT
+		session.table_num,
+		session.day_slot,
+		CONCAT(r.first_name,' ',r.last_name) AS reviewer,
+		CONCAT(au1.first_name,' ',au1.last_name) AS attendee1,
+		CONCAT(au2.first_name,' ',au2.last_name) AS attendee2,
+		CONCAT(au3.first_name,' ',au3.last_name) AS attendee3,
+		CONCAT(au4.first_name,' ',au4.last_name) AS attendee4,
+		CONCAT(au5.first_name,' ',au5.last_name) AS attendee5,
+		CONCAT(au6.first_name,' ',au6.last_name) AS attendee6
 	FROM
-		`session` AS s
-
-	JOIN reviewers AS r
-	ON (
-		r.reviewer_id = s.reviewer_id
-	)
-
-	JOIN attendees AS a
-	ON (
-		a.attendee_id = s.attendee_id1
-		OR
-		a.attendee_id = s.attendee_id2
-		OR
-		a.attendee_id = s.attendee_id3
-		OR
-		a.attendee_id = s.attendee_id4
-		OR
-		a.attendee_id = s.attendee_id5
-		OR
-		a.attendee_id = s.attendee_id6
-	)
-
-	JOIN users AS ur
-	ON (
-		ur.user_id = r.user_id
-	)
-
-	JOIN users AS ua
-	ON (
-		ua.user_id = a.user_id
-	)
-
+		session
+		JOIN reviewers ON session.reviewer_id = reviewers.reviewer_id
+		JOIN users r ON reviewers.user_id = r.user_id
+		LEFT JOIN attendees a1 ON session.attendee_id1 = a1.attendee_id
+		LEFT JOIN users au1 ON a1.user_id = au1.user_id
+		LEFT JOIN attendees a2 ON session.attendee_id2 = a2.attendee_id
+		LEFT JOIN users au2 ON a2.user_id = au2.user_id
+		LEFT JOIN attendees a3 ON session.attendee_id3 = a3.attendee_id
+		LEFT JOIN users au3 ON a3.user_id = au3.user_id
+		LEFT JOIN attendees a4 ON session.attendee_id4 = a4.attendee_id
+		LEFT JOIN users au4 ON a4.user_id = au4.user_id
+		LEFT JOIN attendees a5 ON session.attendee_id5 = a5.attendee_id
+		LEFT JOIN users au5 ON a5.user_id = au5.user_id
+		LEFT JOIN attendees a6 ON session.attendee_id6 = a6.attendee_id
+		LEFT JOIN users au6 ON a6.user_id = au6.user_id
 	WHERE
-		s.registration_period_id = '". $registrationPeriodId ."'
-		". $typeSql ."
-
-	ORDER BY
-		s.table_num ASC
-	");
-
-	$total = count($total);
-
-	$sessions = framework::getMany("
-		SELECT
-			s.day_slot AS day_slot,
-			s.table_num AS table_num,
-			s.reviewer_id AS reviewer_id,
-			ur.first_name AS reviewer_firstname,
-			ur.last_name AS reviewer_lastname,
-			ua.first_name AS attendee_firstname,
-			ua.last_name AS attendee_lastname
-		FROM
-			`session` AS s
-
-		JOIN reviewers AS r
-		ON (
-			r.reviewer_id = s.reviewer_id
-		)
-
-		JOIN attendees AS a
-		ON (
-			a.attendee_id = s.attendee_id1
-			OR
-			a.attendee_id = s.attendee_id2
-			OR
-			a.attendee_id = s.attendee_id3
-			OR
-			a.attendee_id = s.attendee_id4
-			OR
-			a.attendee_id = s.attendee_id5
-			OR
-			a.attendee_id = s.attendee_id6
-		)
-
-		JOIN users AS ur
-		ON (
-			ur.user_id = r.user_id
-		)
-
-		JOIN users AS ua
-		ON (
-			ua.user_id = a.user_id
-		)
-
-		WHERE
-			s.registration_period_id = '". $registrationPeriodId ."'
-			". $typeSql ."
-
-		ORDER BY
-			s.table_num ASC
-
-		LIMIT ". $offset .",". $limit ."
-		");
-
-	$entries = array();
-
-	foreach($sessions as $session) {
-		$daySlot	= $session["day_slot"];
-		$reviewerId	= $session["reviewer_id"];
-		$tableNum	= $session["table_num"];
-
-		$key = $reviewerId;
-
-		$attendee	= $session["attendee_lastname"] . ", " . $session["attendee_firstname"];
-
-		if(!isset($entries[$key])) {
-
-			$reviewer	= $session["reviewer_lastname"] . ", " . $session["reviewer_firstname"];
-
-			$entries[$key] = array(
-				"table_num"	=> $tableNum,
-				"reviewer_name" => $reviewer,
-				"dayslot" => $daySlot,
-				"attendees" => array(
-					$attendee
-				)
-			);
-		} else {
-
-			$entries[$key]["attendees"][] = $attendee;
-
-		}
+		session.registration_period_id = 1 ";
+		
+	if(isset($request["session"])) {
+		$session_sql .= "AND session.day_slot = '" . $request["session"] . "' ";
 	}
-
-	$pagination	= "";
-	$maxPages	= floor( $total / $limit );
-
-	for($i = 1; $i <= $maxPages; $i++) {
-		if($page == $i) {
-			$pagination .= " " . $i . " ";
-		} else {
-			$_day = "";
-
-			if(isset($request["day"])) {
-				$_day = "&day=" . $request["day"];
-			}
-
-			$pagination .= " <a href=\"?page=masterSchedule". $_day ."&p=". $i ."\">". $i ."</a> ";
-		}
-	}
-
-	$day = (isset($request["day"])) ? ucwords($request["day"]) : "";
-
-	echo "<h1 style=\"padding-left: 15px;\">Master Schedule ". (($day == "") ? "" : " - " . $day) ."</h1>";
-	echo "<div style=\"text-align:center;\">". $pagination ."</div>";
-	echo "<hr />";
-	echo "<div class=\"schedule\">";
-
-	foreach($entries as $entry) {
-
-		$day = substr($entry["dayslot"], 0, 1);
-		$time = substr($entry["dayslot"], 1);
-
-		// determine day
+		
+	$session_sql .=
+	"ORDER BY
+		session.day_slot,
+		session.table_num";
+	
+	$sessions = framework::getMany($session_sql);
+	
+	foreach($sessions as $session)
+	{
+		// Build friendly descriptions for day time slot
+		$day = substr($session["day_slot"], 0, 1);
+		$time = substr($session["day_slot"], 1);
+		
 		if($day == "F") {
 			$day = "Friday";
 		} else {
 			$day = "Saturday";
 		}
-
-		// determine time
+		
 		switch($time) {
 			case "1":
 				$time = "9:00am - 11:00am";
@@ -213,36 +82,25 @@
 				$time = "TBD";
 				break;
 		}
-
-
-		echo <<< HTML
-
-				<div class="section">
-					<h2>{$entry["reviewer_name"]}</h2>
-					<p class="table" style="margin-left:-20px;">Table: {$entry["table_num"]} on {$day} from {$time}</p>
-					<hr/>
-					<ul>
-HTML;
-		foreach($entry["attendees"] as $attendee) {
-			echo <<< HTML
-						<li>
-							<p class="name">{$attendee}</p>
-							<!--<p class="time">1:00pm</p>-->
-						</li>
-HTML;
-		}
-
-		echo <<< HTML
-					</ul>
-				</div>
-HTML;
-
-	}
-
-	echo "</div>";
+		
+		// Write out this session entry
+		?>
+		<div class="section">
+		<h2><?php echo $session["reviewer"]; ?></h2>
+		<p class="table" style="margin-left:-20px;">
+			<?php echo "Table: " . $session["table_num"] . " on " . $day . " from " . $time; ?>
+		</p>
+		<hr/>
+		<ul>
+			<li><p class="name"><?php echo $session["attendee1"] ?></p></li>
+			<li><p class="name"><?php echo $session["attendee2"] ?></p></li>
+			<li><p class="name"><?php echo $session["attendee3"] ?></p></li>
+			<li><p class="name"><?php echo $session["attendee4"] ?></p></li>
+			<li><p class="name"><?php echo $session["attendee5"] ?></p></li>
+			<li><p class="name"><?php echo $session["attendee6"] ?></p></li>
+		</ul>
+		</div>
+		<?php
+	};
 ?>
-
-<div class="pdf">
-	<a class="button" href="index.php" onclick="return false;"><</a>
-	<p><a href="index.php" onclick="return false;">Print this Schedule (to download schedule, print as a PDF).</a></p>
 </div>
